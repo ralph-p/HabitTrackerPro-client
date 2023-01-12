47f75aeeef715ea3ecd8032d242cb1eeecb59964
ralph-p <mr.pereira91@gmail.com>
2023-01-12 09:14:59 -0500

    added ui framework

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from 'next/app'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabase] = useState(() => createBrowserSupabaseClient())
   return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-      <Component {...pageProps} />
-    </SessionContextProvider>
+    <ChakraProvider>
+      <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </ChakraProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { Flex, Heading, VStack } from '@chakra-ui/react'
 import Main from '../component/Main'
 
 const Home = () => {
@@ -7,15 +8,19 @@ const Home = () => {
   const supabase = useSupabaseClient()
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      <h1>Resolution tracker</h1>
-      <h3>A simple web app to help you keep track of your various resolutions</h3>
-      {!session ? (
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
-      ) : (
-        <Main />
-      )}
-    </div>
+    <Flex height="100vh" alignItems="center" justifyContent="center" background="blackAlpha.900">
+      <Flex direction="column" padding="10">
+        <VStack spacing={2}>
+          <Heading as='h1' size="xl">Resolution tracker</Heading>
+          <Heading as='h3' size='md'>A simple web app to help you keep track of your various resolutions</Heading>
+        </VStack>
+        {!session ? (
+          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
+        ) : (
+          <Main />
+        )}
+      </Flex>
+    </Flex>
   )
 }
 
