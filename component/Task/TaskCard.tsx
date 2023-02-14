aa967008c8a4995147713f501a69146bca67c2b0
ralph-p <mr.pereira91@gmail.com>
2023-02-14 10:14:13 -0500

    fixed import issues

--- component/Task/TaskCard.tsx
+++ component/Task/TaskCard.tsx
@@ -1,6 +1,6 @@
 import { Card, CardBody, CardHeader, Heading, Text, HStack, useDisclosure, IconButton, Box } from '@chakra-ui/react'
 import React, { useContext } from 'react'
-import { Task } from '../../hooks/useTask.hooks';
+import { Task } from '../../hooks/types/task';
 import moment from "moment"
 import { getCardTheme, seconds, lastUpdated } from '../../utils/task.utils';
 import { SettingsIcon } from '@chakra-ui/icons';
@@ -10,16 +10,6 @@ type Props = {
 
 export const TaskCard = ({ task }: Props) => {
 
-  // const lastUpdated = () => {
-  //   const duration = moment.duration(task.lastUpdated, seconds)
-  //   if (duration.days() > 0) {
-  //     return `Updated: ${duration.days()}d ${duration.hours()}h ago`
-  //   }
-  //   if (duration.hours() > 0) {
-  //     return `Updated: ${duration.hours()}h ${duration.minutes()}m ago`
-  //   }
-  //   return `Updated: ${duration.minutes()}m ago`
-  // }
   const { cardColor } = getCardTheme(task.lastUpdated)
 
 
@@ -30,19 +20,6 @@ export const TaskCard = ({ task }: Props) => {
           <Heading size="md" color="blackAlpha.800">
             {task.name}
           </Heading>
-          {/* <Text color="blackAlpha.700">
-            <IconButton
-              aria-label={'open-task-modal'}
-              size='sm'
-              icon={<SettingsIcon />}
-              variant='ghost'
-              colorScheme='teal'
-              border='0px'
-              // onClick={onOpen}
-              as="a"
-              href={`/task/${task.id}`}
-            />
-          </Text> */}
         </HStack>
       </CardHeader>
       <CardBody>
